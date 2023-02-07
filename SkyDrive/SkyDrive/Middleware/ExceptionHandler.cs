ce24c6755edb103a2e9aca692fa991333b43368a
5Ka <tolkoigri10@mail.ru>
2023-02-07 12:54:26 +0300

    Added exception middleware

---
+++ SkyDrive/SkyDrive/Middleware/ExceptionHandler.cs
@@ -0,0 +1,59 @@
+﻿using SkyDrive.Exceptions;
+using SkyDrive.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace SkyDrive.Middleware
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleException(context, exception);
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception exception)
+        {
+            var statusCode = (int)HttpStatusCode.InternalServerError;
+
+            switch (exception)
+            {
+                case ArgumentNullException:
+                case EntityNotFoundException:
+                    statusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case ArgumentException:
+                    statusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case OperationCanceledException:
+                    statusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+
+            var result = JsonSerializer.Serialize(new ExceptionModel
+            {
+                StatusCode = statusCode,
+                Message = exception.Message
+            });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
