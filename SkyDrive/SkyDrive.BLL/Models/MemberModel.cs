5d4f76dbee318a27faedf47862fbef9a8abe7fee
5Ka <73385569+5Ka-me@users.noreply.github.com>
2023-02-10 10:34:35 +0300

    Added Models and ViewModels. Tried to create my own mapper (#11)

---
+++ SkyDrive/SkyDrive.BLL/Models/MemberModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SkyDrive.BLL.Models
+{
+    public class MemberModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public ICollection<EventModel>? Events { get; set; }
+    }
+}
---
+++ SkyDrive/SkyDrive/ViewModels/InstructorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyDrive.ViewModels
+{
+    public class InstructorViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int Experience { get; set; }
+
+        public ICollection<EventViewModel>? Events { get; set; }
+    }
+}
