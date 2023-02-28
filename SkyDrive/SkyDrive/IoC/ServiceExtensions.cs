0e6f5621b55bcc21668f1980cdeef81314ebf19f
5Ka <tolkoigri10@mail.ru>
2023-02-28 15:12:50 +0300

    Added authentication

--- SkyDrive/SkyDrive/IoC/ServiceExtensions.cs
+++ SkyDrive/SkyDrive/IoC/ServiceExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using SkyDrive.Authentication;
 using SkyDrive.Validators;
 
 namespace SkyDrive.IoC
@@ -8,6 +11,53 @@ namespace SkyDrive.IoC
         public static IServiceCollection AddValidators(this IServiceCollection services)
         {
             services.AddValidatorsFromAssemblyContaining<EventValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth0(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dev-ih5zsl10t5jsta03.eu.auth0.com/";
+                options.Audience = "AufZero";
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwaggerAuthentication(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "openid", "Open Id" }
+                            },
+                            AuthorizationUrl = new Uri(configuration["Authentication:Domain"] +
+                                                       "authorize?audience=" +
+                                                       configuration["Authentication:Audience"])
+                        }
+                    }
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
 
             return services;
         }
